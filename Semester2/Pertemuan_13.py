e499dfdfa49d5b6e3a7f575a4087474433db5191
Adam Gumilang <80865879+adam-gbyte@users.noreply.github.com>
2024-06-06 15:54:16 +0700

    Update Pertemuan_13.py

--- Semester2/Pertemuan_13.py
+++ Semester2/Pertemuan_13.py
@@ -1,41 +1,41 @@
-# while True:
-#     try:
-#         inputan = int(input('Masukkan Umur : '))
-#         print(f'Umur anda : {inputan}')
-#         break
-#     except ValueError:
-#         print('MASUKKAN ANGKA BUKAN HURUF!!!')
+while True:
+    try:
+        inputan = int(input('Masukkan Umur : '))
+        print(f'Umur anda : {inputan}')
+        break
+    except ValueError:
+        print('MASUKKAN ANGKA BUKAN HURUF!!!')
 
 
-# x = 5
-# y = 0
-# try:
-#     hasil = x / y
-#     print(f'{x} * {y} = {hasil}')
-# except ZeroDivisionError:
-#     print('ORA BISA DIBAGI 0')
+x = 5
+y = 0
+try:
+    hasil = x / y
+    print(f'{x} * {y} = {hasil}')
+except ZeroDivisionError:
+    print('ORA BISA DIBAGI 0')
 
 
-# list_angka = [1, 2, 3, 4]
-# try:
-#     index = 6
-#     print(list_angka[index])
-# except IndexError:
-#     print(f'Tidak ada index ke {index}')
+list_angka = [1, 2, 3, 4]
+try:
+    index = 6
+    print(list_angka[index])
+except IndexError:
+    print(f'Tidak ada index ke {index}')
 
 
-# def angka_string(num1, num2, string):
-#     try:
-#         result = num1 / num2
-#         input_str = int(string)
-#         print(result)
-#         print(input_str)
-#     except ZeroDivisionError:
-#         print('Tidak bisa dibagi 0')
-#     except ValueError:
-#         print('Tidak bisa di convert ke INT')
-# angka_string(2, 0, 'abcd')
-# angka_string(2, 2, 'abcd')
+def angka_string(num1, num2, string):
+    try:
+        result = num1 / num2
+        input_str = int(string)
+        print(result)
+        print(input_str)
+    except ZeroDivisionError:
+        print('Tidak bisa dibagi 0')
+    except ValueError:
+        print('Tidak bisa di convert ke INT')
+angka_string(2, 0, 'abcd')
+angka_string(2, 2, 'abcd')
 
 while True:
     try:
